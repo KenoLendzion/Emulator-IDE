f09261e9b8818f5cba850212e37dce19e10859df
kenolendzion <kenolendzion@gmail.com>
2022-11-20 12:19:28 +0100

    AboutWindowViewModel Initial.

---
+++ EmulatorGUI/MVVM/ViewModel/AboutWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Diagnostics;
+
+namespace EmulatorGUI.MVVM.ViewModel
+{
+    public class AboutWindowViewModel
+    {
+
+        public RelayCommand HyperlinkRequestNavigateCommand { get; set; }
+
+        public AboutWindowViewModel()
+        {
+
+            HyperlinkRequestNavigateCommand = new RelayCommand(OpenGithubRepositoryViaURL);
+        }
+
+        private void OpenGithubRepositoryViaURL()
+        {
+            string githubUri = "https://github.com/KenoLendzion/Emulator-IDE";
+
+            var process = new ProcessStartInfo(githubUri)
+            {
+                UseShellExecute = true
+            };
+
+            Process.Start(process);
+        }
+    }
+}
